08cae551d2fa79f3e83ff69b6263233258e7cdae
Rishabh Mishra <rishabhmishra.81e@gmail.com>
2025-08-12 16:48:38 +0530

    use of env

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  UncontrolledTooltip
-} from 'reactstrap';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
-import { DefaultEditor } from 'react-simple-wysiwyg';
-import Avatar from 'react-avatar';
+import React, { useEffect, useState } from "react";
+import { Navbar, NavbarBrand, UncontrolledTooltip } from "reactstrap";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { DefaultEditor } from "react-simple-wysiwyg";
+import Avatar from "react-avatar";
 
-import './App.css';
+import "./App.css";
 
-const WS_URL = 'ws://127.0.0.1:8000';
+const WS_URL = process.env.REACT_APP_WS_URL;
+console.log("WebSocket URL:", WS_URL);
 
 function isUserEvent(message) {
   let evt = JSON.parse(message.data);
-  return evt.type === 'userevent';
+  return evt.type === "userevent";
 }
 
 function isDocumentEvent(message) {
   let evt = JSON.parse(message.data);
-  return evt.type === 'contentchange';
+  return evt.type === "contentchange";
 }
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const { sendJsonMessage, readyState } = useWebSocket(WS_URL, {
     onOpen: () => {
-      console.log('WebSocket connection established.');
+      console.log("WebSocket connection established.");
     },
     share: true,
     filter: () => false,
     retryOnError: true,
-    shouldReconnect: () => true
+    shouldReconnect: () => true,
   });
 
   useEffect(() => {
-    if(username && readyState === ReadyState.OPEN) {
+    if (username && readyState === ReadyState.OPEN) {
       sendJsonMessage({
         username,
-        type: 'userevent'
+        type: "userevent",
       });
     }
   }, [username, sendJsonMessage, readyState]);
@@ -49,21 +46,20 @@ function App() {
         <NavbarBrand href="/">Real-time document editor</NavbarBrand>
       </Navbar>
       <div className="container-fluid">
-        {username ? <EditorSection/>
-            : <LoginSection onLogin={setUsername}/> }
+        {username ? <EditorSection /> : <LoginSection onLogin={setUsername} />}
       </div>
     </>
   );
 }
 
 function LoginSection({ onLogin }) {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   useWebSocket(WS_URL, {
     share: true,
-    filter: () => false
+    filter: () => false,
   });
   function logInUser() {
-    if(!username.trim()) {
+    if (!username.trim()) {
       return;
     }
     onLogin && onLogin(username);
@@ -77,11 +73,18 @@ function LoginSection({ onLogin }) {
             <p className="account__name">Hello, user!</p>
             <p className="account__sub">Join to edit the document</p>
           </div>
-          <input name="username" onInput={(e) => setUsername(e.target.value)} className="form-control" />
+          <input
+            name="username"
+            onInput={(e) => setUsername(e.target.value)}
+            className="form-control"
+          />
           <button
             type="button"
             onClick={() => logInUser()}
-            className="btn btn-primary account__btn">Join</button>
+            className="btn btn-primary account__btn"
+          >
+            Join
+          </button>
         </div>
       </div>
     </div>
@@ -89,15 +92,17 @@ function LoginSection({ onLogin }) {
 }
 
 function History() {
-  console.log('history');
+  console.log("history");
   const { lastJsonMessage } = useWebSocket(WS_URL, {
     share: true,
-    filter: isUserEvent
+    filter: isUserEvent,
   });
   const activities = lastJsonMessage?.data.userActivity || [];
   return (
     <ul>
-      {activities.map((activity, index) => <li key={`activity-${index}`}>{activity}</li>)}
+      {activities.map((activity, index) => (
+        <li key={`activity-${index}`}>{activity}</li>
+      ))}
     </ul>
   );
 }
@@ -105,13 +110,13 @@ function History() {
 function Users() {
   const { lastJsonMessage } = useWebSocket(WS_URL, {
     share: true,
-    filter: isUserEvent
+    filter: isUserEvent,
   });
   const users = Object.values(lastJsonMessage?.data.users || {});
-  return users.map(user => (
+  return users.map((user) => (
     <div key={user.username}>
       <span id={user.username} className="userInfo" key={user.username}>
-        <Avatar name={user.username} size={40} round="20px"/>
+        <Avatar name={user.username} size={40} round="20px" />
       </span>
       <UncontrolledTooltip placement="top" target={user.username}>
         {user.username}
@@ -125,12 +130,12 @@ function EditorSection() {
     <div className="main-content">
       <div className="document-holder">
         <div className="currentusers">
-          <Users/>
+          <Users />
         </div>
-        <Document/>
+        <Document />
       </div>
       <div className="history-holder">
-        <History/>
+        <History />
       </div>
     </div>
   );
@@ -139,21 +144,19 @@ function EditorSection() {
 function Document() {
   const { lastJsonMessage, sendJsonMessage } = useWebSocket(WS_URL, {
     share: true,
-    filter: isDocumentEvent
+    filter: isDocumentEvent,
   });
 
-  let html = lastJsonMessage?.data.editorContent || '';
+  let html = lastJsonMessage?.data.editorContent || "";
 
   function handleHtmlChange(e) {
     sendJsonMessage({
-      type: 'contentchange',
-      content: e.target.value
+      type: "contentchange",
+      content: e.target.value,
     });
   }
 
-  return (
-    <DefaultEditor value={html} onChange={handleHtmlChange} />
-  );
+  return <DefaultEditor value={html} onChange={handleHtmlChange} />;
 }
 
 export default App;
